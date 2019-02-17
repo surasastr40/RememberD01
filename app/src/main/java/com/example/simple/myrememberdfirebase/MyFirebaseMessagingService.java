98e3f9c5fb19fca9a5bb1eef11d79e9f1695f6e8
Aukkarathep <58160501@go.buu.ac.th>
2019-02-17 14:01:33 +0700

    dae commit

--- app/src/main/java/com/example/simple/myrememberdfirebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/simple/myrememberdfirebase/MyFirebaseMessagingService.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
@@ -19,35 +20,23 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
         showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
-//        showNotification(remoteMessage.getNotification());
+
     }
 
     private void showNotification(String title, String message) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"MyNotification")
-                .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark_normal)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setAutoCancel(true);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"channel_id");
+        builder.setSmallIcon(R.drawable.common_google_signin_btn_icon_dark_normal);
+        builder.setContentTitle(title);
+        builder.setContentText(message);
+        builder.setAutoCancel(true);
+        builder.setLights(Color.RED, 1000, 300);
 
         NotificationManagerCompat manager = NotificationManagerCompat.from(this);
-        manager.notify(999,builder.build());
+        manager.notify(0,builder.build());
     }
-
-    //    private void showNotification(RemoteMessage.Notification notification) {
-//        Intent intent = new Intent(this,MainActivity.class);
-//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//        PendingIntent pendingIntent = PendingIntent.getActivities(this,0, new Intent[]{intent},PendingIntent.FLAG_ONE_SHOT);
-//
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-//                .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark_normal)
-//                .setContentTitle(notification.getTitle())
-//                .setContentText(notification.getBody())
-//                .setAutoCancel(true)
-//                .setContentIntent(pendingIntent);
-//
-//        NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-//        notificationManager.notify(0,builder.build());
-//    }
-
 
 }//MyFirebaseMessagingService
